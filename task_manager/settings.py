5c1c539066974ea140d398c18796caed09695e03
ilia <iliarassolov@gmail.com>
2025-04-24 14:14:43 +0400

    24/04/2025

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,9 +26,9 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Include BOOTSTRAP5_FOLDER in path
-# BOOTSTRAP5_FOLDER = os.path.abspath(os.path.join(BASE_DIR, "..", "django_bootstrap5"))
-# if BOOTSTRAP5_FOLDER not in sys.path:
-#     sys.path.insert(0, BOOTSTRAP5_FOLDER)
+BOOTSTRAP5_FOLDER = os.path.abspath(os.path.join(BASE_DIR, "..", "django_bootstrap5"))
+if BOOTSTRAP5_FOLDER not in sys.path:
+    sys.path.insert(0, BOOTSTRAP5_FOLDER)
 
 
 # Quick-start development settings - unsuitable for production
@@ -153,10 +153,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# BOOTSTRAP5 = {
-#     "error_css_class": "django_bootstrap5-error",
-#     "required_css_class": "django_bootstrap5-required",
-#     "javascript_in_head": True,
-# }
+BOOTSTRAP5 = {
+    "error_css_class": "django_bootstrap5-error",
+    "required_css_class": "django_bootstrap5-required",
+    "javascript_in_head": True,
+}
 
 # STATIC_ROOT = BASE_DIR / "staticfiles"
