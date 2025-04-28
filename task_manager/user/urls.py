ce74cc2209fcc6d21ca134ab470ab27f20d87b0e
ilia <iliarassolov@gmail.com>
2025-04-28 10:59:47 +0400

    28/04/2025

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     # path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     # path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     # path('<int:id>/', CommentArticleView.as_view(), name='comment_create',),
-    # path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
 ]
